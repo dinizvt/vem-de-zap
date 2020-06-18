065d030f225ef019eb6635b27011d566380f8918
Vitor Diniz <vitordiniz2010@hotmail.com>
2020-06-18 20:22:10 -0300

    mobile

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
+function isMobile(){
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+        return true;
+    }
+    return false;
+}
+
 function sendMsg(phone, msg){
+    var plt = isMobile() ? 'api' : 'web';
     var msgEncoded = encodeURIComponent(msg);
     var phone = phone.match(/\d+/g).join('');
     console.log(phone);
     if(phone.length===11 || phone.length ===10){
         phone = "55"+phone;
     }
-    console.log(phone)
-    var url = `https://web.whatsapp.com/send?phone=${phone}&text=${msgEncoded}`;
+    var url = `https://${plt}.whatsapp.com/send?phone=${phone}&text=${msgEncoded}`;
     window.open(url);
 }
-$(document).ready(() => {
-    $("#msg-form").on('submit', (e)=>{
-        e.preventDefault();
-        var phone = $("#phone").val();
-        var msg = $("#msg").val();
-        sendMsg(phone, msg);
-    });
-});
+
+window.onload = () => {
+    var msgForm = document.getElementById('msg-form');
+    var phone = document.getElementById("phone");
+    var msg = document.getElementById("msg");
+
+    msgForm.onsubmit = (e) =>{
+        sendMsg(phone.value, msg.value)
+    };
+};
